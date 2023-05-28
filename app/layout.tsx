48f66bbfb012f851939d875f62071bb928340b7b
Hanif Nur Amrullah <hanif@ruangguru.com>
2023-05-28 07:53:39 +0700

    add filter topic and not found view

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,13 +40,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body>
         <div className="flex h-full flex-1 flex-col">
           <div className="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <input id="drawer-1" type="checkbox" className="drawer-toggle" />
             <AudioProvider>
               <div className="drawer-content flex flex-col">
                 <header className="navbar border-b border-solid border-gray-200 gap-2 bg-base-100">
                   <div className="flex-none">
                     <label
-                      htmlFor="my-drawer-2"
+                      htmlFor="drawer-1"
                       className="btn-ghost btn-square btn drawer-button lg:hidden"
                     >
                       <HiMenuAlt2 size={20} />
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               </div>
             </AudioProvider>
             <div className="drawer-side border-r border-gray-200">
-              <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+              <label htmlFor="drawer-1" className="drawer-overlay"></label>
               <div className="bg-base-100 w-56">
                 <div className="p-4">
                   <Link href="/" className="btn-ghost btn text-xl normal-case">
--- app/radio/radio-list.tsx
+++ app/radio/radio-list.tsx
@@ -5,6 +5,7 @@ import { TrackInfo } from "./entity";
 import { RadioItem } from "./radio-item";
 import MiniSearch, { SearchResult } from "minisearch";
 import { useAudioContext } from "../audio-context";
+import { HiOutlineDocumentSearch } from "react-icons/hi";
 
 export type RadioListProps = {
   items: TrackInfo[];
@@ -46,6 +47,45 @@ const teachers = [
   },
 ];
 
+const topics = [
+  {
+    name: "Adab",
+    keyword: "adab",
+  },
+  {
+    name: "Akhlak",
+    keyword: "akhlak",
+  },
+  {
+    name: "Tajwid",
+    keyword: "tajwid",
+  },
+  {
+    name: "Sholat",
+    keyword: "sholat",
+  },
+  {
+    name: "Pendidikan anak",
+    keyword: "tarbiyah anak buah hati",
+  },
+  {
+    name: "Amal",
+    keyword: "harta amal timbangan infaq",
+  },
+  {
+    name: "Hadist",
+    keyword: "hadist sunnah",
+  },
+  {
+    name: "Kisah",
+    keyword: "kisah",
+  },
+  {
+    name: "Pemerintah",
+    keyword: "pemerintah",
+  },
+];
+
 const sortRadios = (
   radios: TrackInfo[] | SearchResult[],
   sortBy: string | undefined
@@ -83,19 +123,23 @@ const search = (radios: TrackInfo[] | SearchResult[], keyword: string) => {
   return miniSearch.search(keyword);
 };
 
+const defaultFilter = {
+  keyword: "",
+  sortBy: "",
+  teacher: "",
+  topic: "",
+};
+
 export function RadioList(props: RadioListProps) {
   const { filterShown = true } = props;
   const { track, play, stop, isLoading } = useAudioContext();
 
-  const [{ keyword, sortBy, teacher }, setFilter] = useState<{
+  const [{ keyword, sortBy, teacher, topic }, setFilter] = useState<{
     keyword: string;
     sortBy: string;
     teacher: string;
-  }>({
-    keyword: "",
-    sortBy: "",
-    teacher: "",
-  });
+    topic: string;
+  }>(defaultFilter);
 
   const setKeyword = (keyword: string) => {
     setFilter((prev) => ({ ...prev, keyword }));
@@ -106,6 +150,13 @@ export function RadioList(props: RadioListProps) {
   const setTeacher = (teacher: string) => {
     setFilter((prev) => ({ ...prev, teacher }));
   };
+  const setTopic = (topic: string) => {
+    setFilter((prev) => ({ ...prev, topic }));
+  };
+
+  const resetFilter = () => {
+    setFilter(defaultFilter);
+  };
 
   const results = useMemo(() => {
     let radios: TrackInfo[] | SearchResult[] = props.items;
@@ -114,49 +165,67 @@ export function RadioList(props: RadioListProps) {
       radios = search(radios, keyword);
     }
 
+    if (topic) {
+      radios = search(radios, topic);
+    }
+
     if (teacher) {
       radios = search(radios, teacher);
     }
 
     return sortRadios(radios, sortBy);
-  }, [props.items, keyword, sortBy, teacher]);
+  }, [props.items, keyword, sortBy, teacher, topic]);
 
   return (
     <div>
       {filterShown ? (
-        <div className="mb-6 grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-4">
+        <div className="mb-6 grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4">
           <input
             type="text"
-            placeholder="Cari..."
-            className="input input-bordered"
+            placeholder="Cari radio / judul..."
+            className="input input-bordered input-sm sm:input-md"
             value={keyword}
             onChange={(event) => setKeyword(event.target.value)}
           />
           <select
-            className="select"
+            className="select select-bordered select-sm sm:select-md"
             onChange={(event) => setSortBy(event.target.value)}
           >
-            <option disabled selected>
-              Urutkan
-            </option>
-            <option value="">Default</option>
+            <option value="">Urutkan</option>
             <option value="most">Pendengar Terbanyak</option>
             <option value="less">Pendengar Tersedikit</option>
           </select>
           <select
-            className="select"
+            className="select select-bordered select-sm sm:select-md"
             onChange={(event) => setTeacher(event.target.value)}
           >
-            <option disabled selected>
-              Pilih Ustadz
-            </option>
-            <option value="">Semua</option>
+            <option value="">Semua Ustadz</option>
             {teachers.map((teacher) => (
               <option key={teacher.keyword} value={teacher.keyword}>
                 {teacher.name}
               </option>
             ))}
           </select>
+          <select
+            className="select select-bordered select-sm sm:select-md"
+            onChange={(event) => setTopic(event.target.value)}
+          >
+            <option value="">Semua Topic</option>
+            {topics.map((item) => (
+              <option key={item.keyword} value={item.keyword}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      ) : null}
+      {results.length === 0 ? (
+        <div className="flex flex-1 flex-col justify-center items-center p-8 bg-base-100 gap-2">
+          <HiOutlineDocumentSearch size={48} />
+          Radio tidak ditemukan
+          <button className="btn btn-primary btn-sm" onClick={resetFilter}>
+            Kembali
+          </button>
         </div>
       ) : null}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
