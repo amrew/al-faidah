08063a3d072bd1a8e06d30cf98061c15a32c8db3
Hanif Nur Amrullah <hanif@ruangguru.com>
2023-05-30 21:22:18 +0700

    add radio syariah url

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -2,9 +2,13 @@ import { get } from "@vercel/edge-config";
 import { getTracks } from "./service";
 import { RadioList } from "./radio-list";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { type: "rii" | "syariah" };
+}) {
   const [radios, teachers, topics] = await Promise.all([
-    getTracks(),
+    getTracks({ type: searchParams.type }),
     get<{ name: string; keyword: string }[]>("teachers"),
     get<{ name: string; keyword: string }[]>("topics"),
   ]);
--- app/radio/service.ts
+++ app/radio/service.ts
@@ -1,17 +1,25 @@
 import { RadioInfo, TrackInfo } from "./entity";
 
-const API_URL = process.env.RII_URL;
+const RII_URL = process.env.RII_URL;
+const SYARIAH_URL = process.env.SYARIAH_URL;
 
-export async function getTracks() {
-  const result = await fetch(`${API_URL}/radio/lrii.php?model=lima`, {
+export async function getTracks(params: { type?: "rii" | "syariah" } = {}) {
+  const { type = "rii" } = params;
+  const url =
+    type === "syariah"
+      ? `${SYARIAH_URL}/rsy/list/v2jsyariah.php?listradio=syariah`
+      : `${RII_URL}/radio/lrii.php?model=lima`;
+
+  const result = await fetch(url, {
     next: { revalidate: 5 },
   });
   const data: TrackInfo[] = await result.json();
+  console.log(data);
   return data;
 }
 
 export async function getRadio(id: string) {
-  const result = await fetch(`${API_URL}/jupuk.php?ambil=radet&id_radet=${id}`);
+  const result = await fetch(`${RII_URL}/jupuk.php?ambil=radet&id_radet=${id}`);
   const data: RadioInfo[] = await result.json();
   return data.length > 0 ? data[0] : undefined;
 }
