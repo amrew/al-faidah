3c9dd0182c7d21c4292a282c565b5d560892c6c0
Hanif Nur Amrullah <hanif@ruangguru.com>
2023-06-03 09:37:12 +0700

    reduce the items rendered

--- components/radio-list.tsx
+++ components/radio-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ModalEmbed } from "./modal-embed";
 import type { TrackInfo } from "./radio-entity";
 import { RadioItem } from "./radio-item";
@@ -11,15 +11,25 @@ export type RadioListProps = {
 
 export function RadioList(props: RadioListProps) {
   const [selectedTrack, setSelectedTrack] = useState<TrackInfo>();
+  const half = Math.floor(props.items.length / 2);
+  const [renderOnClient, setRenderOnClient] = useState(false);
+
+  // reduce the item rendered on the server side.
+  useEffect(() => {
+    setRenderOnClient(true);
+  }, []);
+
   return (
     <>
-      {props.items?.map((item) => (
-        <RadioItem
-          key={item.id}
-          item={item}
-          onEmbedClick={() => setSelectedTrack(item)}
-        />
-      ))}
+      {props.items?.map((item, index) =>
+        index < half || renderOnClient ? (
+          <RadioItem
+            key={item.id}
+            item={item}
+            onEmbedClick={() => setSelectedTrack(item)}
+          />
+        ) : null
+      )}
       <ModalEmbed track={selectedTrack} />
     </>
   );
