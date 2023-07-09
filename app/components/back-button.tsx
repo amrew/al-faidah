d27d2e3dc3ddd3f931dde9097c9d6eb8bc77aa77
Hanif Nur Amrullah <hanif@ruangguru.com>
2023-07-09 15:34:31 +0700

    update mobile layout

--- app/components/back-button.tsx
+++ app/components/back-button.tsx
@@ -2,12 +2,17 @@ import { useNavigate } from "react-router-dom";
 
 import { BiArrowBack } from "react-icons/bi";
 
-export function BackButton() {
+type Props = {
+  withText?: boolean;
+};
+
+export function BackButton(props: Props) {
+  const { withText = true } = props;
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
   return (
     <button type="button" className="btn-ghost btn" onClick={() => goBack()}>
-      <BiArrowBack size={20} /> Kembali
+      <BiArrowBack size={20} /> {withText ? "Kembali" : ""}
     </button>
   );
 }
--- app/components/shared-layout.tsx
+++ app/components/shared-layout.tsx
@@ -3,6 +3,7 @@ import { MemberNavigation } from "./member-navigation";
 import { Player } from "./player";
 import { Link, NavLink } from "@remix-run/react";
 import { BiHome, BiRadio, BiSearch } from "react-icons/bi";
+import { BackButton } from "./back-button";
 
 export type SharedLayoutProps = {
   footer?: ReactNode;
@@ -17,23 +18,28 @@ export function SharedLayout(props: PropsWithChildren<SharedLayoutProps>) {
   return (
     <div className={contentClassname}>
       <header className="navbar border-b border-solid gap-2 bg-base-200 border-b-base-300 sticky top-0 left-0 right-0 z-50">
-        <div className="flex-none">
+        <div
+          className={`flex-none ${hasSearchComponent ? "hidden sm:flex" : ""}`}
+        >
           <Link to="/" className={`btn-ghost btn text-xl normal-case`}>
             Al Faidah
           </Link>
         </div>
-        <div className="flex-none">
+        {hasSearchComponent ? (
+          <div className="sm:hidden">
+            <BackButton withText={false} />
+          </div>
+        ) : null}
+        <div className="flex-none hidden sm:flex">
           <ul className="menu menu-sm menu-horizontal bg-base-200 rounded-box gap-2">
             <li>
               <NavLink to="/">
-                <BiHome size={20} />{" "}
-                <span className="hidden md:inline">Beranda</span>
+                <BiHome size={20} /> Beranda
               </NavLink>
             </li>
             <li>
               <NavLink to="/radio">
-                <BiRadio size={20} />{" "}
-                <span className="hidden md:inline">Radio</span>
+                <BiRadio size={20} /> Radio
               </NavLink>
             </li>
           </ul>
@@ -42,7 +48,10 @@ export function SharedLayout(props: PropsWithChildren<SharedLayoutProps>) {
         <div
           className={`flex-1 justify-end ${hasSearchComponent ? "hidden" : ""}`}
         >
-          <Link to="/cari" className="btn btn-ghost btn-circle btn-sm">
+          <Link
+            to="/cari"
+            className={`btn btn-ghost btn-circle btn-sm hidden sm:flex `}
+          >
             <BiSearch />
           </Link>
         </div>
@@ -58,6 +67,17 @@ export function SharedLayout(props: PropsWithChildren<SharedLayoutProps>) {
       <div className="fixed bottom-0 left-0 w-full">
         {props.footer}
         <Player />
+        <div className="btm-nav btm-nav-sm border-t border-t-base-200 sm:hidden">
+          <NavLink to="/" className="bg-white">
+            <BiHome />
+          </NavLink>
+          <NavLink to="/radio" className="bg-white">
+            <BiRadio />
+          </NavLink>
+          <NavLink to="/cari" className="bg-white">
+            <BiSearch />
+          </NavLink>
+        </div>
       </div>
     </div>
   );
