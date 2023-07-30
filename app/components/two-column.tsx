1ba0f3710b47f12b5a6317fe07d2c97344a78207
Hanif Nur Amrullah <hanif@ruangguru.com>
2023-07-30 10:14:15 +0700

    change row reverse layout to row

--- app/components/two-column.tsx
+++ app/components/two-column.tsx
@@ -6,8 +6,9 @@ export type TwoColumnProps = {
 export function TwoColumn(props: TwoColumnProps) {
   return (
     <div
-      className={`flex flex-col md:flex-row-reverse justify-end p-4 md:px-8 gap-4 lg:gap-16`}
+      className={`flex flex-col md:flex-row justify-end p-4 md:px-8 gap-4 lg:gap-16`}
     >
+      <div className="w-full md:w-7/12 xl:w-8/12">{props.left}</div>
       {props.right ? (
         <div className="w-full md:w-5/12 xl:w-4/12">
           <div className="flex flex-col gap-4 sticky top-16 py-4">
@@ -15,7 +16,6 @@ export function TwoColumn(props: TwoColumnProps) {
           </div>
         </div>
       ) : null}
-      <div className="w-full md:w-7/12 xl:w-8/12">{props.left}</div>
     </div>
   );
 }
